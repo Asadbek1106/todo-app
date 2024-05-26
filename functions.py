0c5ffc2fec1a0819699365f841912411c958d3c8
asadbekkuziev <mrasa0710@gmail.com>
2024-05-26 08:59:48 +0100

    Creating a simple gui

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+
 def get_todo(filepath):
     """ Read a text file and return the list of
     to_do items.
@@ -8,6 +9,5 @@ def get_todo(filepath):
 
 
 def write_todos(filepath, todos_arg):
-    """ Write the to_do items in the text file."""
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
